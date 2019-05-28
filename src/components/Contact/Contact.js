cda608a99df5debf741b9aca82f6a98f5d35e1f4
Bhekani Khumalo <bkhumalo@esri-southafrica.com>
2019-05-28 18:20:07 +0200

    added form to html

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -60,39 +60,35 @@ const Contact = (props) => {
       <form
         name="rsvp"
         method="POST"
-        netlify-honeypot="bot-field"
-        data-netlify="true"
-        // action="/success"
-        data-netlify-recaptcha="true"
+        action="/success"
         onSubmit={handleSubmit}
         >
         <input type="hidden" name="form-name" value="rsvp" />
         <input type="hidden" name="bot-field" />
 
         <label className="name">
-          Name: {name}
+          Name:
           <input type="text" id="name" name="name" value={name} onChange={handleChange} required />
         </label>
 
         <label className="email">
-          Email: {email}
+          Email:
           <input type="email" id="email" name="email" value={email} onChange={handleChange} required />
         </label>
 
         <label className="number">
-          Number of guests: {number}
+          Number of guests:
           <input type="number" id="number" name="number" value={number} onChange={handleChange} required />
         </label>
 
         <label className="message">
-          Leave a message for the couple: {message}
+          Leave a message for the couple:
           <textarea name="message" id="message" rows="4" value={message} onChange={handleChange} required />
         </label>
 
-        <div data-netlify-recaptcha="true"></div>
-          <button className="button form-button" type="submit">
-            Send
-          </button>
+        <button className="button form-button" type="submit">
+          Send
+        </button>
       </form>
     </section>
   );
